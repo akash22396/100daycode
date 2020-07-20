5da3102cbaa8e488ca7698292366fa3d57c1e5df
Akash Kumar Singh <akash.singh22396@gmail.com>
2020-07-20 13:06:18 +0530

    Update Day_2_ReverseString.js

--- JavaScript/Day_2_ReverseString.js
+++ JavaScript/Day_2_ReverseString.js
@@ -24,3 +24,27 @@ specialChar.forEach(dt=>{
 return revString
 }
 console.log(reverseOnlyLetters('hello-world-!'))
+
+/*********************** Reverse String by k Value  ************************/
+let s='abcdefg',k=2;
+var reverseStr = function(s, k) {
+   let data = s.toLowerCase().split('')
+  let arr1=[];
+  let arr2=[];
+  for(let i=0;i<data.length;i+=k){
+    let arr= []
+    for(let j=0;j<k;j++){
+      arr.push(data[i+j])
+    }
+    arr1.push(arr)
+  }
+  for(let i=0;i<arr1.length;i++){
+      if(i%2!==0){
+       arr2.push(arr1[i])
+      }else{
+        let dt = arr1[i].reverse();
+        arr2.push(dt)      
+      }
+  }
+  return arr2.join('').replace(/,/g,'')
+};
